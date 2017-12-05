4fd510a4ef1b41bd6e18ca9870da2bfc249ac392
Bhirawa Mbani <bhirmbani@gmail.com>
2017-12-05 14:18:43 +0700

    adding register input

--- src/components/userOnboarding/index.js
+++ src/components/userOnboarding/index.js
@@ -7,24 +7,54 @@ import {
   Item,
   Input,
   Label,
+  Button,
+  Text,
 } from 'native-base';
 
-const userOnboarding = () => (
-  <Container>
-    <Header />
-    <Content>
-      <Form>
-        <Label>Email</Label>
-        <Item rounded>
-          <Input />
-        </Item>
-        <Label>Password</Label>
-        <Item rounded last>
-          <Input />
-        </Item>
-      </Form>
-    </Content>
-  </Container>
-);
+class userOnboarding extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      retype: '',
+    };
+  }
+  render() {
+    return (
+      <Container>
+        <Header />
+        <Content>
+          <Form>
+            <Label>Enter your email</Label>
+            <Item rounded>
+              <Input
+                onChangeText={email => this.setState({ email })}
+                value={this.state.email}
+              />
+            </Item>
+            <Label>Type your password</Label>
+            <Item rounded last>
+              <Input
+                onChangeText={password => this.setState({ password })}
+                value={this.state.password}
+              />
+            </Item>
+            <Label>Retype your password</Label>
+            <Item rounded last>
+              <Input
+                onChangeText={retype => this.setState({ retype })}
+                value={this.state.retype}
+              />
+            </Item>
+            <Button rounded primary >
+              <Text>SIGNUP</Text>
+            </Button>
+          </Form>
+        </Content>
+      </Container>
+    );
+  }
+}
 
 export default userOnboarding;
