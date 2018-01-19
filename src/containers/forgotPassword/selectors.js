0452c5b6f69254f57899c2f7b1b48641e1e9d338
Krisrp <kristoforusrp@gmail.com>
2018-01-20 01:04:14 +0700

    reselect as getter from redux store

---
+++ src/containers/forgotPassword/selectors.js
@@ -0,0 +1,38 @@
+import { createSelector } from 'reselect';
+
+/**
+ * Subscribe to main reducer with key forgotpassword
+ *
+ * @return object
+ */
+export const selectForgotPasswordReducer = () => state => state.get('forgotPassword');
+
+/**
+ * Getter for showLoading in reducer.
+ *
+ * @return object
+ */
+export const getShowLoading = () => createSelector(
+  selectForgotPasswordReducer(),
+  state => state.get('showLoading'),
+);
+
+/**
+ * Getter for message in reducer.
+ *
+ * @return object
+ */
+export const getMessage = () => createSelector(
+  selectForgotPasswordReducer(),
+  state => state.get('message'),
+);
+
+/**
+ * Getter for isSuccess in reducer.
+ *
+ * @return object
+ */
+export const getIsSuccess = () => createSelector(
+  selectForgotPasswordReducer(),
+  state => state.get('isSuccess'),
+);
