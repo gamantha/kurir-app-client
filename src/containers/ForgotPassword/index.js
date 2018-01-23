942931b345a4c58285d03203c499fc92102da7b9
Krisrp <kristoforusrp@gmail.com>
2018-01-24 05:42:01 +0700

    modify forgot password containers

---
+++ src/containers/ForgotPassword/index.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import {
+  Container,
+  Header,
+  Content,
+  Form,
+  Item,
+  Input,
+  Label,
+  Button,
+  Text,
+  Toast,
+  Root
+} from 'native-base';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import * as actions from './actions';
+import * as selectors from './selectors';
+// import VerifCodeInput from '../../components/verifCode';
+
+class ForgotPasswordInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: ''
+    };
+  }
+
+  onPressSendEmail = (email) => {
+    this.props.forgotPassword({ email });
+  };
+
+  setEmail = (email) => {
+    this.props.setEmail(email);
+  };
+
+  render() {
+    // if (this.props.msgReducer.forgotPassMsg.isSuccess) {
+    //   return <VerifCodeInput email={this.state.email} />;
+    // }
+    return (
+      <Container>
+        <Header />
+        <Content>
+          <Form>
+            <Label>Enter your email</Label>
+            <Item rounded>
+              <Input
+                onChangeText={(email) => this.setEmail(email)}
+                value={this.props.email}
+                autoCapitalize="none"
+              />
+            </Item>
+            <Button
+              rounded
+              primary
+              onPress={() => {
+                this.onPressSendEmail(this.state.email);
+              }}
+            >
+              <Text>SEND EMAIL</Text>
+            </Button>
+            <Text>{null ? 'loading' : 'tidak loading'}</Text>
+            <Text>{null ? 'sukses' : 'tidak sukses'}</Text>
+          </Form>
+        </Content>
+      </Container>
+    );
+  }
+}
+
+const mapStateToProps = () =>
+  createStructuredSelector({
+    isShowLoading: selectors.getShowLoading(),
+    message: selectors.getMessage(),
+    email: selectors.getEmail()
+  });
+
+export default connect(mapStateToProps, actions)(ForgotPasswordInput);
