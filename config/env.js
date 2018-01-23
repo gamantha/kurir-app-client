48799d729f1b78cf7924583bc734cf40c3a0d265
Krisrp <kristoforusrp@gmail.com>
2018-01-24 05:40:38 +0700

    update configuration

--- config/env.js
+++ config/env.js
@@ -1,4 +1,5 @@
 const config = require('./development').default;
 
-export const API_BASE_URL = config.DEV_API_URL_LOCALHOST;
+const API_BASE_URL = config.DEV_API_URL_LOCALHOST;
 // export const API_BASE_URL = config.DEV_API_URL_HEROKU;
+export default API_BASE_URL;
