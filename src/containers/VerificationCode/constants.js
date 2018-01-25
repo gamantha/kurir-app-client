4007047dfcc0c9260c0b8b65d96083c69f0ab5e4
Krisrp <kristoforusrp@gmail.com>
2018-01-26 00:17:40 +0700

    move to container

---
+++ src/containers/VerificationCode/constants.js
@@ -0,0 +1,4 @@
+const IS_LOADING_VERIFICATION_CODE = 'src/containers/VerificationCode/IS_LOADING_VERIFICATION_CODE';
+const VERIFICATION_STATUS = 'src/containers/VerificationCode/VERIFICATION_STATUS';
+const VERIFICATION_MESSAGE = 'src/containers/VerificationCode/VERIFICATION_MESSAGE';
+const SET_VERIFICATION_CODE = 'src/containers/VerificationCode/SET_VERIFICATION_CODE';
