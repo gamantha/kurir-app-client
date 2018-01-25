e6193562e45c80510b4285cf3a3e7ea9bd96b365
Krisrp <kristoforusrp@gmail.com>
2018-01-26 06:10:14 +0700

    added email dan password validation helper

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -2,11 +2,21 @@ import React from 'react';
 
 import { connect } from 'react-redux';
 
+// Email Validation
+export function validateEmail(inputvalue) {
+  const pattern = /^([a-zA-Z0-9_.-])+@([a-zA-Z0-9_.-])+\.([a-zA-Z])+([a-zA-Z])+/;
+  return !!pattern.test(inputvalue);
+}
+
+// Name Validation
+export function validateName(inputvalue) {
+  const pattern = /([^a-zA-Z0-9_\ -])/g;
+  return !pattern.test(inputvalue);
+}
+
 export const connectWithStore = (store, WrappedComponent, ...args) => {
   const ConnectedWrappedComponent = connect(...args)(WrappedComponent);
-  return function (props) {
+  return function(props) {
     return <ConnectedWrappedComponent {...props} store={store} />;
   };
 };
-
-const test = () => {};
