368a91ff2fd11bcaaea689dbe2ea4d010bdb9db3
Krisrp <kristoforusrp@gmail.com>
2018-01-26 00:37:11 +0700

    update reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,11 +5,18 @@ import { combineReducers } from 'redux-immutable';
 import forgotPasswordReducer from '../containers/ForgotPassword/reducer';
 import userRegisterReducer from '../containers/UserRegister/reducer';
 import userLoginReducer from '../containers/UserLogin/reducer';
+import verificationCodeReducer from '../containers/VerificationCode/reducer';
 
+/**
+ * Combine all reducers each containers
+ * Into one Javascript Object
+ * @type {Maps}
+ */
 const rootReducer = combineReducers({
   userRegister: userRegisterReducer,
   userLogin: userLoginReducer,
-  forgotPassword: forgotPasswordReducer
+  forgotPassword: forgotPasswordReducer,
+  verificationCode: verificationCodeReducer
 });
 
 export default rootReducer;
