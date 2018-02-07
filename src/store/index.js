8a6f4201a5ab649e63bb3bb0645cc99ae93d0c91
Krisrp <kristoforusrp@gmail.com>
2018-02-08 05:59:21 +0700

    additional setting

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,15 @@
 import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
 import { createStore, applyMiddleware, compose } from 'redux';
+import { persistStore } from 'redux-persist';
 
 import rootReducer from '../reducers';
 import rootSaga from '../sagas';
 
 const sagaMiddleWare = createSagaMiddleware();
-// const storeWithMiddlewares = compose(applyMiddleware(sagaMiddleWare, logger))(createStore);
 
 export const store = createStore(rootReducer, compose(applyMiddleware(logger, sagaMiddleWare)));
 
 sagaMiddleWare.run(rootSaga);
 
-// Later install persistStore on this
-export const persistStore = () => ({});
+// export const persistor = persistStore(store);
