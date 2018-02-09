9721fc452e0146cfc2af978d1ff3a115057073ac
Krisrp <kristoforusrp@gmail.com>
2018-02-09 10:16:31 +0700

    add hook functions

--- src/components/Onboarding/onEnter.js
+++ src/components/Onboarding/onEnter.js
@@ -1,6 +1,6 @@
 import SInfo from 'react-native-sensitive-info';
 import { Actions } from 'react-native-router-flux';
 
-export default function onEnter(props) {
-  SInfo.getItem('accessToken', {}).then((token) => (token ? Actions.profile() : Actions.onboarding()));
+export default function onEnter() {
+  SInfo.getItem('accessToken', {}).then((token) => (token ? Actions.profile() : null));
 }
