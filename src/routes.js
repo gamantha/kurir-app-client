71b737abdf21ddae6333ed1b0e089bf522e4e77d
Krisrp <kristoforusrp@gmail.com>
2018-02-10 22:07:40 +0700

    Added routes

---
+++ src/routes.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { StackNavigator } from 'react-navigation';
+
+import SplashScreen from './components/SplashScreen';
+import Onboarding from './components/Onboarding';
+import UserRegister from './containers/UserRegister';
+import UserLogin from './containers/UserLogin';
+import UserProfile from './containers/UserProfile';
+import ForgotPassword from './containers/ForgotPassword';
+
+export const Kurir = StackNavigator(
+    {
+        Splash: {
+            screen: SplashScreen,
+            navigationOptions: {
+                header: null
+            }
+        },
+        Onboard: {
+            screen: Onboarding,
+            navigationOptions: {
+                header: null
+            }
+        },
+        Register: {
+            screen: UserRegister,
+            navigationOptions: {
+                header: null
+            }
+        },
+        Login: {
+            screen: UserLogin,
+            navigationOptions: {
+                header: null
+            }
+        },
+        Password: {
+            screen: ForgotPassword,
+            navigationOptions: {
+                header: null
+            }
+        },
+        Profile: {
+            screen: UserProfile,
+            navigationOptions: {
+                header: null
+            }
+        }
+    },
+    //config
+    {
+        initialRouteName: 'Register'
+    }
+);
