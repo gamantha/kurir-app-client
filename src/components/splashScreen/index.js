ce4d73545cac07c17f86a521817029efcd8f1310
Krisrp <kristoforusrp@gmail.com>
2018-02-10 22:12:04 +0700

    Initiated animation on splash screen

--- src/components/splashScreen/index.js
+++ src/components/splashScreen/index.js
@@ -1,12 +1,41 @@
 /* eslint-disable */
-import React from 'react';
-import { AppLoading, Asset } from 'expo';
+import React, { Component } from 'react';
+import { Animated, Image, ActivityIndicator, Easing } from 'react-native';
 
-class SplashScreen extends React.Component {
-    constructor(props) {
-        super(props);
+import { images } from '../../assets';
+
+class SplashScreen extends Component {
+    constructor() {
+        super();
         this.state = {
-            isReady: false
+            loading: true
         };
+        this.divideScale = new Animated.Value(0);
+    }
+
+    componentDidMount() {
+        setTimeout(() => this.props.navigation.navigate('Onboard'), 3000);
+    }
+
+    moveit = () => {
+        this.divideScale.setValue(0.3);
+        Animated.timing(this.divideScale, {
+            toValue: 1,
+            duration: 1000,
+            easing: Easing.bounce
+        }).start(() => this.moveit());
+    };
+
+    render() {
+        return (
+            <Animated.View>
+                <Image
+                    style={{ width: '100%', height: '100%' }}
+                    source={images.splash}
+                />
+            </Animated.View>
+        );
     }
 }
+
+export default SplashScreen;
