ec28a4d32b10846262eab1e112a73ad2a0620de3
Krisrp <kristoforusrp@gmail.com>
2018-02-10 22:05:27 +0700

    Added assets

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+export const images = {
+    splash: require('./images/splash.png'),
+    facebook: require('./images/facebook.png'),
+    google: require('./images/googleplus.png'),
+    logo: require('./images/kurirlogo.png')
+};
