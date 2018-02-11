c3ca6db0b21ff917f4577a83352af4507b94abe2
Krisrp <kristoforusrp@gmail.com>
2018-02-12 06:02:20 +0700

    Centralized assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,5 +3,8 @@ export const images = {
     facebook: require('./images/facebook.png'),
     google: require('./images/googleplus.png'),
     logo: require('./images/kurirlogo.png'),
-    baseline: require('./images/baseline.png')
+    baseline: require('./images/baseline.png'),
+    traveler: require('./images/splash-1.png'),
+    handoff: require('./images/splash-2.png'),
+    trackPackage: require('./images/splash-3.png')
 };
