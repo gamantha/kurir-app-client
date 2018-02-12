6263f0a1d10ceaf1a8dd083756df6edd1c3e330a
Krisrp <kristoforusrp@gmail.com>
2018-02-12 07:52:42 +0700

    Added asset for dashboard

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,5 +6,8 @@ export const images = {
     baseline: require('./images/baseline.png'),
     traveler: require('./images/splash-1.png'),
     handoff: require('./images/splash-2.png'),
-    trackPackage: require('./images/splash-3.png')
+    trackPackage: require('./images/splash-3.png'),
+    kurirheader: require('./images/kurirheader.png'),
+    box: require('./images/box.png'),
+    rocket: require('./images/rocket.png')
 };
