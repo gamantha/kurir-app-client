1cac6af40ff936e1cdbf8a93184f6eb2f989bd1e
Krisrp <kristoforusrp@gmail.com>
2018-02-20 08:36:38 +0700

    Updated regex pattern

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,11 +1,11 @@
 // Email Validation
 export function validateEmail(inputvalue) {
-  const pattern = /^([a-zA-Z0-9_.-])+@([a-zA-Z0-9_.-])+\.([a-zA-Z])+([a-zA-Z])+/;
-  return !!pattern.test(inputvalue);
+    const pattern = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9_.-]{2,}))+\.(([a-zA-Z]{1,}))$/;
+    return !!pattern.test(inputvalue);
 }
 
 // Name Validation
 export function validateName(inputvalue) {
-  const pattern = /([^a-zA-Z0-9_ -])/g;
-  return !pattern.test(inputvalue);
+    const pattern = /([^a-zA-Z0-9_ -])/g;
+    return !pattern.test(inputvalue);
 }
