d6dd534997c87a23154d9bd0914ef52e00dbc299
Krisrp <kristoforusrp@gmail.com>
2018-02-21 08:43:27 +0700

    Initial integration with fbsdk

--- android/app/src/main/java/id/kurir/MainApplication.java
+++ android/app/src/main/java/id/kurir/MainApplication.java
@@ -3,17 +3,25 @@ package id.kurir;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import br.com.classapp.RNSensitiveInfo.RNSensitiveInfoPackage;
+import com.facebook.CallbackManager;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -25,6 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FBSDKPackage(mCallbackManager),
             new VectorIconsPackage(),
             new RNSensitiveInfoPackage()
       );
