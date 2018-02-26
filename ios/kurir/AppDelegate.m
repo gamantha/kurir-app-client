9d7fdf159ae28a944b93dee90b3970fe834bf3d8
Krisrp <kristoforusrp@gmail.com>
2018-02-27 01:37:02 +0700

    Completed google oauth

--- ios/kurir/AppDelegate.m
+++ ios/kurir/AppDelegate.m
@@ -12,6 +12,7 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <RNGoogleSignin/RNGoogleSignin.h>
 
 @implementation AppDelegate
 
@@ -46,9 +47,18 @@
                                                                 openURL:url
                                                       sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]
                                                              annotation:options[UIApplicationOpenURLOptionsAnnotationKey]
-                  ];
+                  ] || [RNGoogleSignin application:application
+                                           openURL:url
+                                 sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]
+                                        annotation:options[UIApplicationOpenURLOptionsAnnotationKey]
+                        ];
   // Add any custom logic here.
   return handled;
 }
 
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+  
+  return [RNGoogleSignin application:application openURL:url sourceApplication:sourceApplication annotation:annotation];
+}
 @end
