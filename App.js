f1337ac21566de9c23a0455aa987220fe945d45f
Pholenk <pholenkadi17@gmail.com>
2018-03-05 23:31:19 +0700

    adjusting app.js for new drawer routing

--- App.js
+++ App.js
@@ -3,7 +3,15 @@ import { View, SafeAreaView } from 'react-native';
 import { connect, Provider } from 'react-redux';
 
 import { PersistGate } from 'redux-persist/integration/react';
-import { store } from './src/store';
+import { store, persistor } from './src/store';
+
+import UserRegister from './src/containers/UserRegister';
+// import SendPackagePage from './src/components/sendPackagePage';
+import UserLogin from './src/containers/UserLogin';
+import Onboarding from './src/components/onboarding';
+import ForgotPassword from './src/containers/ForgotPassword';
+import ChangePassword from './src/components/changePassword';
+import EditProfile from './src/components/EditProfile';
 
 import { Kurir } from './src/routes';
 
