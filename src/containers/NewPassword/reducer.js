ceaa62d966b72f35aafa98d187a7e0de1f29f14e
mazsam <arifinsam.sam@gmail.com>
2018-03-12 00:31:23 +0700

    reducer newpassword

--- src/containers/NewPassword/reducer.js
+++ src/containers/NewPassword/reducer.js
@@ -1,7 +1,7 @@
 import { fromJS } from 'immutable';
 
 import {
-    SHOW_LOADING,
+    IS_LOADING,
     NEW_PASSWORD_SUCCESS,
     NEW_PASSWORD_ERROR,
     TEXT_INPUT_FOCUS,
@@ -18,7 +18,11 @@ const initialState = fromJS({
     inputTextFocus: {
         password: '#FFFFFF',
         rePassword: '#FFFFFF'
-    }
+    },
+    isLoadingNewPassword: false,
+    newPasswordSuccess: null,
+    newPasswordError: ''
+
 });
 
 export default function newPasswordReducer(state = initialState, action) {
@@ -27,6 +31,14 @@ export default function newPasswordReducer(state = initialState, action) {
             return state.set(['newPasswordField', action.field], action.value);
         case TEXT_INPUT_FOCUS:
             return state.setIn(['inputTextFocus', action.field], action.style);
+        case IS_LOADING:
+            return state.set('isLoadingNewPassword', action.status);
+        case NEW_PASSWORD_SUCCESS:
+            return state.set('newPasswordSuccess', action.payload);
+        case NEW_PASSWORD_ERROR:
+            return state.set('newPasswordError', action.payload);
+        case CLEAR_ERROR_MESSAGE:
+            return state.set('newPasswordError', '')
         default:
             return state;
     }
@@ -47,4 +59,13 @@ export const textInputFocus = (field, style) => ({
 export const newPasswordFlow = payload => ({
     type: NEW_PASSWORD,
     payload
+});
+
+export const setIsNewPassword = status => ({
+    type: IS_LOADING,
+    status
+});
+
+export const clearErrorMessage = () => ({
+    type: CLEAR_ERROR_MESSAGE
 });
