f59391ae61269e9f67ac1e167c90b59c4bc08f05
mazsam <arifinsam.sam@gmail.com>
2018-03-12 20:04:57 +0700

    root saga

--- src/sagas.js
+++ src/sagas.js
@@ -4,7 +4,8 @@ import userLoginSagas from './containers/UserLogin/sagas';
 import registerUserSagas from './containers/UserRegister/sagas';
 import forgotPasswordSagas from './containers/ForgotPassword/sagas';
 import verifycationCodeSagas from './containers/VerificationCode/sagas';
+import newPasswordSagas from './containers/NewPassword/sagas';
 
 export default function* rootSaga() {
-  yield all([ userLoginSagas, registerUserSagas, forgotPasswordSagas, verifycationCodeSagas ]);
+  yield all([ userLoginSagas, registerUserSagas, forgotPasswordSagas, verifycationCodeSagas, newPasswordSagas ]);
 }
