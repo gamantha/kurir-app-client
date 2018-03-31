c92f7616c364b57b656d5dd234c39c8dbcda0148
mazsam <arifinsam.sam@gmail.com>
2018-03-31 18:01:50 +0700

    Constants sendpackage

---
+++ src/containers/SendPackage/constants.js
@@ -0,0 +1,5 @@
+export const SEND_PACKAGE               = 'src/containers/SEND_PACKAGE';
+export const SEND_PACKAGE_ERROR         = 'src/containers/SendPackage/SEND_PACKAGE_ERROR';
+export const CLEAR_ERROR_MESSAGE        = 'src/containers/SendPackage/CLEAR_ERROR_MESSAGE';
+export const UPDATE_SEND_PACKAGE        = 'src/containers/UPDATE_SEND_PACKAGE';
+export const SEND_PACKAGE_SUCCESS       = 'src/containers/SendPackage/SEND_PACKAGE_SUCCESS';
