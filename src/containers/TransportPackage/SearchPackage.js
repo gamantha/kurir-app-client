8b563623b57f59d54e94bd5a7e17a598446b0a44
mazsam <arifinsam.sam@gmail.com>
2018-04-01 15:07:05 +0700

    Seacrh a package ui

--- src/containers/TransportPackage/SearchPackage.js
+++ src/containers/TransportPackage/SearchPackage.js
@@ -29,16 +29,14 @@ class SearchPackage extends Component {
 
     render() {
         return (
-            <ScrollView>
                 <View
                     style={{
                         flex: 1,
                         flexDirection: 'column',
-                        justifyContent: 'space-around',
                         backgroundColor: '#fff'
                     }}
                 >
-                    <View style={{ flex: 0.05 }} />
+                    {/* <View style={{ flex: 0.05 }} /> */}
                     <View
                         style={{
                             flex: 0.8,
@@ -56,15 +54,11 @@ class SearchPackage extends Component {
                             ]}
                         >
                             <Text style={{ marginTop: 20, marginBottom: 5 }}>
-                                From
+                                Origin
                             </Text>
-                            <View style={styles.inputTextContainer}>
+                            <View style={styles.SectionStyle}>
+                                <Image source={images.origin} style={styles.InputImageStyle} />
                                 <TextInput
-                                    style={styles.inputText}
-                                    onFocus={}
-                                    onBlur={}
-                                    onChangeText={}
-                                    value={}
                                     autoCapitalize="none"
                                     autoCorrect={false}
                                     underlineColorAndroid="transparent"
@@ -82,15 +76,11 @@ class SearchPackage extends Component {
                             ]}
                         >
                             <Text style={{ marginTop: 10, marginBottom: 5 }}>
-                                To
+                                Destination
                             </Text>
-                            <View style={styles.inputTextContainer}>
+                            <View style={styles.SectionStyle}>
+                                <Image source={images.destination} style={styles.InputImageStyle} />
                                 <TextInput
-                                    style={styles.inputText}
-                                    onFocus={}
-                                    onBlur={}
-                                    onChangeText={}
-                                    value={}
                                     autoCapitalize="none"
                                     autoCorrect={false}
                                     underlineColorAndroid="transparent"
@@ -100,138 +90,34 @@ class SearchPackage extends Component {
 
                         <View
                             style={{
-                                marginTop: 10,
-                                marginLeft: 30,
-                                marginRight: 30
+                                flex: 2,
+                                alignItems: 'center'
                             }}
-                        >
-                            <View>
-                                <Text>
-                                    <Text style={{ fontWeight: 'bold' }}>
-                                        note:
-                                    </Text>{' '}
-                                    please make sure your package can fit into 1
-                                    standard luggage and no more than 20KG's
-                                </Text>
-                            </View>
-                        </View>
-
-                        <View
-                            style={[
-                                styles.container,
-                                {
-                                    marginTop: 10,
-                                    marginLeft: 20,
-                                    marginRight: 20,
-                                    flexDirection: 'row',
-                                    justifyContent: 'flex-end',
-                                    alignItems: 'center'
-                                }
-                            ]}
                         >
                             <TouchableOpacity
-                                onPress={this.handlePress}
-
-                                style={[
-                                    styles.touchAbleButton,
-                                    { width: '45%' }
-                                ]}
-                            >
-                                <Text style={styles.textButton}>
-                                    Calculate Price
-                                </Text>
-                            </TouchableOpacity>
-                        </View>
-
-                        <View
-                            style={{
-                                flexDirection: 'column',
-                                marginTop: 20,
-                                backgroundColor: '#EFEFEF'
-                            }}
-                        >
-                            <View
                                 style={{
-                                    marginTop: 10,
-                                    marginLeft: 30,
-                                    marginRight: 30,
-                                    justifyContent: 'flex-end'
+                                    width: '83%',
+                                    top: 20,
+                                    height: 50,
+                                    backgroundColor: '#D8283B',
+                                    borderRadius: 30,
+                                    justifyContent: 'center'
                                 }}
                             >
                                 <Text
                                     style={{
-                                        fontWeight: 'bold',
-                                        fontSize: 24,
-                                        textAlign: 'right'
+                                        textAlign: 'center',
+                                        fontSize: 16,
+                                        color: '#FFFFFF',
+                                        fontWeight: 'bold'
                                     }}
                                 >
-                                    Approx cost: Rp. 12.000
+                                    Search for package
                                 </Text>
-                            </View>
-                            <View
-                                style={{
-                                    marginTop: 10,
-                                    marginLeft: 30,
-                                    marginRight: 30,
-                                    marginBottom: 20
-                                }}
-                            >
-                                <Text style={{ fontSize: 16 }}>
-                                    <Text style={{ fontWeight: 'bold' }}>
-                                        note:
-                                    </Text>{' '}
-                                    Items to be shipped will be inspected and
-                                    repackaged at our drop point facility. Final
-                                    price maybe different that shown
-                                </Text>
-                            </View>
-                        </View>
-
-                        <View
-                            style={[
-                                styles.container,
-                                {
-                                    margin: 20,
-                                    flexDirection: 'row',
-                                    justifyContent: 'space-around',
-                                    alignItems: 'center'
-                                }
-                            ]}
-                        >
-                            <TouchableOpacity
-                                onPress={() =>
-                                    this.props.screenProps.rootNavigation.dispatch(
-                                        resetAction
-                                    )
-                                }
-                                style={[
-                                    styles.touchAbleButton,
-                                    {
-                                        borderColor: '#B5B5B5',
-                                        backgroundColor: '#B5B5B5',
-                                        width: '45%'
-                                    }
-                                ]}
-                            >
-                                <Text style={styles.textButton}>Back</Text>
-                            </TouchableOpacity>
-                            <TouchableOpacity
-                                onPress={() =>
-                                    this.props.navigation.navigate(
-                                        'PackageInfo'
-                                    )
-                                }
-                                style={[
-                                    styles.touchAbleButton,
-                                    { width: '45%' }
-                                ]}
-                            >
-                                <Text style={styles.textButton}>Ship Item</Text>
                             </TouchableOpacity>
                         </View>
                     </View>
                 </View>
-            </ScrollView>
         );
     }
 }
